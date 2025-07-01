17b59b245f9672b3215039b8dffe8932f08c587a
Avery Babcock <averybabcock@ascend-rw01.ten.osc.edu>
2025-06-30 23:46:50 -0400

    Add Assignment 2 files and update README

---
+++ .ipynb_checkpoints/linear_regression_python-checkpoint.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# This notebook demonstrates a simple linear regression analysis using Python to model Salary based on Years of Experience.
+
+# In[2]:
+
+
+import pandas as pd
+dataset = pd.read_csv("regression_data.csv")
+
+
+# In[3]:
+
+
+import matplotlib.pyplot as plt
+plt.scatter(dataset["YearsExperience"], dataset["Salary"], color="red")
+
+
+# In[4]:
+
+
+from sklearn.linear_model import LinearRegression
+model = LinearRegression()
+model.fit(dataset[["YearsExperience"]], dataset[["Salary"]])
+
+
+# In[5]:
+
+
+plt.plot(dataset["YearsExperience"], model.predict(dataset[["YearsExperience"]]), color="blue")
+plt.title("Salary vs Experience")
+plt.xlabel("Years of Experience")
+plt.ylabel("Salary")
+plt.show()
+
+
+# In[6]:
+
+
+model.score(dataset[["YearsExperience"]], dataset[["Salary"]])  # R-squared
+
+
+# In[ ]:
+
+
+
+
---
+++ .ipynb_checkpoints/linear_regression_r-checkpoint.r
@@ -0,0 +1,19 @@
+dataset <- read.csv("regression_data.csv")
+
+plot(dataset$YearsExperience, dataset$Salary, col="red")
+
+model <- lm(Salary ~ YearsExperience, data=dataset)
+
+install.packages("ggplot2")
+
+library(ggplot2)
+ggplot() +
+  geom_point(aes(x = dataset$YearsExperience, y = dataset$Salary), colour = 'red') +
+  geom_line(aes(x = dataset$YearsExperience, y = predict(model, newdata = dataset)), colour = 'blue') +
+  ggtitle('Salary vs Experience') +
+  xlab('Years of experience') +
+  ylab('Salary')
+
+summary(model)
+
+
